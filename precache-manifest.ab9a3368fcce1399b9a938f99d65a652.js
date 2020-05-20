72dfa2b0989d2d3ba0bb4c2926e7803c886c306d
Dan Horne <d.horne92@live.co.uk>
2020-05-20 12:29:43 +0100

    Updates

---
+++ precache-manifest.ab9a3368fcce1399b9a938f99d65a652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6186a2a5face63d6608156a5c14f039",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "93925534a491e1cc56c1",
+    "url": "/tictactoe/static/css/main.20b5a7d8.chunk.css"
+  },
+  {
+    "revision": "885087fbc97e2343c05a",
+    "url": "/tictactoe/static/js/2.25f9344f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.25f9344f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93925534a491e1cc56c1",
+    "url": "/tictactoe/static/js/main.90727c9d.chunk.js"
+  },
+  {
+    "revision": "8cd3739712635f2cd5b8",
+    "url": "/tictactoe/static/js/runtime-main.148deea7.js"
+  }
+]);
